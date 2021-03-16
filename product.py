773fa6e7002d87bc43aeab334805e1ca9514d37e
saton <1410567345@qq.com>
2021-03-16 10:03:04 +0800

    老师导入 以及 音频导入

---
+++ product.py
@@ -0,0 +1,107 @@
+import pandas as pd
+from openpyxl.reader.excel import load_workbook
+from builtins import int
+import datetime
+import pymysql
+def importExcelToMysql(cur, path,test=0):      
+    sheet = pd.read_excel(path,keep_default_na=False,sheet_name=[0])
+    for sqlstr in sheet[0].values:
+        #status = 0 break
+        status = str(sqlstr[8])
+        if status == '0':
+                continue        
+        teachernumber=str(sqlstr[2]).strip()
+        # 校验有没有这个编号的 音频AA
+#         print(teachernumber)
+        cur.execute("select id,number from product where number = (%s)",teachernumber)
+        #拿ok_teacher_id
+        data = cur.fetchone(); #(1, '男声1号')
+
+        check_teachernumber = ''
+        if data:
+            check_teachernumber = str(data[1]).strip()
+        if  check_teachernumber == teachernumber:
+            print('有重复的音频')
+            continue
+        
+        else:
+            time='{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+            #拿teacher_id
+            #ok
+            if test == 1:
+                ok_teacher_id = str(sqlstr[10])
+                cur.execute("select * from temp_table where ok_teacher_id = (%s)",ok_teacher_id)
+                temp_data = cur.fetchone();
+                if temp_data is  None:
+                    print(ok_teacher_id+'ccccccccccccccccccccccccccc')
+            if test == 2:
+                #go
+                go_teacher_id = str(sqlstr[10])
+                cur.execute("select * from temp_table where go_teacher_id = (%s)",go_teacher_id)
+                temp_data = cur.fetchone();
+                if temp_data is  None:
+                    print(go_teacher_id+'ccccccccccccccccccccccccccc')
+            
+
+            teacher_id = str(temp_data[1])
+#             print(ok_teacher_id+'------'+teacher_id)
+#                     id	sort	number	type	lang	sex	age	mood	status	audio_url	teacher_id   
+            valuestr = [str(sqlstr[2]).strip(),str(sqlstr[1]), str(sqlstr[8]), str(sqlstr[9]),teacher_id,time]
+            cur.execute("insert into product(number,sort,status,audio_url,teacher_id,created_at) values(%s,%s,%s,%s,%s,%s)", valuestr)
+            product_id = getLastId(cur)
+#             print([product_id])
+            #五个属性↓ 4 5 6 7 8
+            t = str(sqlstr[3]).strip()
+            cur.execute("select id from product_type where name = (%s) and category_id = 2",t)
+            t_data  = cur.fetchone()
+            t_productstr = [product_id,t_data[0]]
+            t_res = cur.execute("insert into product_type_intermedial(product_id,product_type_id) values(%s,%s)",t_productstr)
+            
+            l = str(sqlstr[4]).strip()
+            cur.execute("select id from product_type where name = (%s) and category_id = 1",l)
+            l_data  = cur.fetchone()
+            l_productstr = [product_id,l_data[0]]
+            l_res = cur.execute("insert into product_type_intermedial(product_id,product_type_id) values(%s,%s)",l_productstr)
+
+            s = str(sqlstr[5]).strip()
+            cur.execute("select id from product_type where name = (%s) and category_id = 3",s)
+            s_data  = cur.fetchone()
+            s_productstr = [product_id,s_data[0]]
+            s_res = cur.execute("insert into product_type_intermedial(product_id,product_type_id) values(%s,%s)",s_productstr)
+            
+            a = str(sqlstr[6]).strip()
+            cur.execute("select id from product_type where name = (%s) and category_id = 4",a)
+            a_data  = cur.fetchone()
+            a_productstr = [product_id,l_data[0]]
+            a_res = cur.execute("insert into product_type_intermedial(product_id,product_type_id) values(%s,%s)",l_productstr)
+            
+            m = str(sqlstr[7]).strip()
+            cur.execute("select id from product_type where name = (%s) and category_id = 5",m)
+            m_data  = cur.fetchone()
+            m_productstr = [product_id,m_data[0]]
+            m_res = cur.execute("insert into product_type_intermedial(product_id,product_type_id) values(%s,%s)",m_productstr)
+            
+            print('insert success'+'----'+str(product_id)+'---属性：'+str(t_res)
+                 +str(l_res)+str(s_res)+str(a_res)+str(m_res))
+def getLastId(cur):
+    cur.execute("select last_insert_id();")
+    data = cur.fetchone();
+    return data[0]
+
+
+if __name__ == '__main__':
+    starttime = datetime.datetime.now()
+#     conn = pymysql.connect(host = '14.18.80.193', user ='root', password ='KsmwE6tK3s5c5nmC', database ='test', charset='utf8')
+    conn = pymysql.connect(host = 'localhost', user ='root', password ='123456', database ='test', charset='utf8')
+    cur = conn.cursor()
+#     input_time=input("输入：输入1 或 2")
+#     importExcelToMysql(cur, "./ok_audio_list.xlsx",1)
+    importExcelToMysql(cur, "./go_audio_list.xlsx",2)
+    cur.close()
+    conn.commit()
+    # 关闭数据库服务器连接，释放内存
+    conn.close()
+    endtime = datetime.datetime.now()
+    x = endtime - starttime
+
+    print(x)
---
+++ teacher.py
@@ -0,0 +1,74 @@
+
+import pandas as pd
+from openpyxl.reader.excel import load_workbook
+from builtins import int
+import datetime
+import pymysql
+def importExcelToMysql(cur, path,test=0):      
+    if test==1:
+        sheet = pd.read_excel(path,keep_default_na=False,sheet_name=[0])
+    #             sheet['okvoices_teacher'].values
+        for sqlstr in sheet[0].values:
+            time='{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+            #         status	avatar	number	nickname	good_at	location	sort	created_at     
+    #             id	头像	善长(描述)	编号	地址	昵称	状态	排序
+            valuestr = [str(sqlstr[7]), str(sqlstr[1]), str(sqlstr[3]).strip(), str(sqlstr[5]), str(sqlstr[2]), str(sqlstr[4]), str(sqlstr[6]),time]
+            cur.execute("insert into teacher(status,avatar,number,nickname,good_at,location,sort,created_at) values(%s,%s,%s,%s,%s,%s,%s,%s)", valuestr)
+            teacher_id = getLastId(cur)
+            #temp_table
+            ok_teacher_id = str(sqlstr[0])
+            teacherstr = [teacher_id,ok_teacher_id]
+            test = cur.execute("insert into temp_table(teacher_id,ok_teacher_id) values(%s,%s)",teacherstr)
+            print('insert success'+'--------'+str(teacher_id))
+    elif test==2:
+        sheet = pd.read_excel(path,keep_default_na=False,sheet_name=[1])
+    #             sheet['goodvoice_teacher'].values
+        for sqlstr in sheet[1].values:
+            #temp_table
+            teachernumber=str(sqlstr[3]).strip()
+            #查询名字是否相同 相同就插入到同一条 teacher_id 的临时表/ 不相同就直接插入 go_teacher_id 与 teacher_id
+            cur.execute("select id,number from teacher where number = (%s)",teachernumber)
+            #拿ok_teacher_id
+            data = cur.fetchone(); #(1, '男声1号')
+            if data:
+                check_teachernumber = str(data[1]).strip()
+            if  check_teachernumber == teachernumber:
+                go_teacher_id = int(sqlstr[0])
+                teacher_id = int(data[0])
+                upteachertemp = [go_teacher_id,teacher_id]
+                cur.execute("update temp_table set go_teacher_id=(%s) where teacher_id = (%s)",upteachertemp)
+                print('update_success'+check_teachernumber+'----'+teachernumber+'----'+str(go_teacher_id)+'--'+str(teacher_id))
+            else:
+                time='{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+                #         status	avatar	number	nickname	good_at	location	sort	created_at     
+                valuestr = [str(sqlstr[7]), str(sqlstr[1]), str(sqlstr[3]).strip(), str(sqlstr[5]), str(sqlstr[2]), str(sqlstr[4]), str(sqlstr[6]),time]
+                cur.execute("insert into teacher(status,avatar,number,nickname,good_at,location,sort,created_at) values(%s,%s,%s,%s,%s,%s,%s,%s)", valuestr)
+                teacher_id = getLastId(cur)
+                go_teacher_id = int(sqlstr[0])
+                teacherstr = [teacher_id,go_teacher_id]
+                cur.execute("insert into temp_table(teacher_id,go_teacher_id) values(%s,%s)",teacherstr)
+                if data:
+                    print('insert_success'+data[1]+'----'+teachernumber)
+                else:
+                    print('insert_success'+'-----'+str(teacher_id)+'----'+teachernumber)
+def getLastId(cur):
+    cur.execute("select last_insert_id();")
+    data = cur.fetchone();
+    return data[0]
+
+
+if __name__ == '__main__':
+    starttime = datetime.datetime.now()
+#     conn = pymysql.connect(host = '14.18.80.193', user ='root', password ='KsmwE6tK3s5c5nmC', database ='test', charset='utf8')
+    conn = pymysql.connect(host = 'localhost', user ='root', password ='123456', database ='test', charset='utf8')
+    cur = conn.cursor()
+#     input_time=input("输入：输入1 或 2")
+    importExcelToMysql(cur, "./ok+gv-teacher.xlsx",2)#先执行 1 再执行 2
+    cur.close()
+    conn.commit()
+    # 关闭数据库服务器连接，释放内存
+    conn.close()
+    endtime = datetime.datetime.now()
+    x = endtime - starttime
+
+    print(x)
